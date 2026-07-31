1fd8adb7d7def04e93f5f0a4185d607801dedab7
akhmy <akhmy.work@mail.ru>
2026-07-31 19:28:30 +0300

    feat(request logger): add skip paths

--- request_logger.go
+++ request_logger.go
@@ -9,9 +9,21 @@ import (
 	"go.uber.org/zap"
 )
 
-func RequestLogger(loggerFunc LoggerFunc) func(http.Handler) http.Handler {
+func RequestLogger(loggerFunc LoggerFunc, skipPaths ...string) func(http.Handler) http.Handler {
+	skipMap := make(map[string]struct{}, len(skipPaths))
+	for _, s := range skipPaths {
+		skipMap[s] = struct{}{}
+	}
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, ok := skipMap[r.URL.Path]
+			if ok {
+				next.ServeHTTP(w, r)
+
+				return
+			}
+
 			logger := loggerFunc(r)
 			r = r.WithContext(addLogger(r.Context(), logger))
 			ww := chimw.NewWrapResponseWriter(w, r.ProtoMajor)
