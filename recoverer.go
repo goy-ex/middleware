48ab2651436ca2541569157d38e0b576aa09796e
akhmy <akhmy.work@mail.ru>
2026-09-06 17:45:18 +0300

    refactor: recoverer returns http.Handler

--- recoverer.go
+++ recoverer.go
@@ -2,18 +2,17 @@ package middleware
 
 import "net/http"
 
-func Recoverer() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				rec := recover()
-				if rec != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte("Internal Server Error"))
-				}
-			}()
+func Recoverer(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			rec := recover()
+			if rec != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Internal Server Error"))
+			}
+		}()
 
-			next.ServeHTTP(w, r)
-		})
-	}
+		next.ServeHTTP(w, r)
+	})
+
 }
